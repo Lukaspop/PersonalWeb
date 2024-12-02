1497d761f3788c715f3479c05d9ad233f2e49d31
Lukas <Luk4sp0p@gmail.com>
2024-12-02 11:22:07 +0100

    update HomePage styles and content for improved layout and readability

--- lukaspop/src/pages/HomePage.tsx
+++ lukaspop/src/pages/HomePage.tsx
@@ -1,9 +1,10 @@
 import styles from './HomePage.module.css';
 export const HomePage = () => {
   return (
-    <div>
-      <h1>Ahoj ty beta-samče</h1>
-      <p>jmenuju se <span className={styles.bolt}>Lukas Pop</span>a jsem majitelem mléčné dráhy.</p>
+    <div className={styles.container}>
+      <h1>Ahoj, ty taško!</h1>
+      <p>jmenuju se <span className={styles.bolt}>Lukas Pop</span> a jsem majitelem <span className={styles.bolt}>Mléčné dráhy</span>.</p>
+      <p> jo a jinak Citroen C3 je <span className={styles.orange}>pomeranč</span>. </p>
     </div>
   );
 };
